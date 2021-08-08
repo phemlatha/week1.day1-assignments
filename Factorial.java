1a6a373e44e939add83dd53f3f0550a679818c1a
phemlatha <88576056+phemlatha@users.noreply.github.com>
2021-08-08 11:35:05 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,18 @@
+package Week1.day1.assingments;
+
+public class Factorial {
+	public static void main(String[] args) {
+
+	int input = 5;
+	int temp = 1;
+	
+	for (int i = 1; i <= input; i++) {
+		System.out.println(i);
+		
+		  temp = temp * i;
+		  
+		  
+		}
+	System.out.println("temp" +temp);
+}
+}
